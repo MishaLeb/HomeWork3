fe2c7fc78a39b6c76022103e41074f47b9577b18
Mike Lebedev <ley3@yandex.ru>
2023-05-18 21:38:19 +0300

    Initial commit

---
+++ src/Exercise1.java
@@ -0,0 +1,8 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        int ticketPriceRub = 10532;
+        int rub_for_bonus = 20;
+        int miles = ticketPriceRub / rub_for_bonus;
+        System.out.println(miles);
+    }
+}
---
+++ src/Exercise2.java
@@ -0,0 +1,15 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        int onAccount = 100;
+        int refill = 1500;
+        int bonus;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        int total_on_account = onAccount + refill +bonus;
+        System.out.println("Бонус:" + bonus);
+        System.out.println("Итоговый счет:" + total_on_account);
+    }
+}
