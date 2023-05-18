9fbe4c5e9d56d87502b5eff65c9a83f863617bde
Mike Lebedev <ley3@yandex.ru>
2023-05-18 21:45:13 +0300

    cheange

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,8 +1,8 @@
 public class Exercise1 {
     public static void main(String[] args) {
         int ticketPriceRub = 10532;
-        int rub_for_bonus = 20;
-        int miles = ticketPriceRub / rub_for_bonus;
+        int rubForBonus = 20;
+        int miles = ticketPriceRub / rubForBonus;
         System.out.println(miles);
     }
 }
